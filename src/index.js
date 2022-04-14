ec6e085d054d09fdf2b36b0ede467fa8857cc09f
UJ DEV <uj@ujdev.com>
2022-04-14 02:20:42 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const hamburgerMenuContainer = document.querySelector('.hamburger-menu');
 const hamburgerIcon = document.querySelector('.hamburger-icon');
 const portfolioContainer = document.querySelector('.portfolio-container');
+const sideMenuLinks = document.querySelector('.side-menu').children;
 const data = '/data.json'
 
 
@@ -9,22 +10,28 @@ hamburgerIcon.addEventListener('click', ()=> {
 })
 
 
+Array.from(sideMenuLinks, item => {
+    item.addEventListener('click', ()=> {
+        hamburgerMenuContainer.classList.remove('toggleMenu')
+    })
+})
+
 
 const portfolioDisplay = async () => {
     const fetchData = await (await fetch(data)).json();
     portfolioContainer.innerHTML = '';
     const portfolioContent = fetchData.map(item => {
-        const {id, img, alt, dataOs, title, description, repo, liveSite} = item;
+        const {id, img, alt, dataOs, title, description, repo, liveSite, target} = item;
         return portfolioContainer.innerHTML += `<figure id=${id} class="portfolio-card" data-aos=${dataOs}>
         <img src=${img} alt=${alt} class="card-img" />
         <figcaption class="card-content">
             <h4> ${title} </h4>
             <p> ${description} </p>
             <footer class="card-btns">
-                <a href=${repo}>
+                <a href=${repo} target=${target}>
                 <img src="/public/images/githubb.png" alt = "#" title="github repo" />
                 </a>
-                <a href=${liveSite}>
+                <a href=${liveSite} target=${target}>
                     <img src="/public/images/link.png" alt = "#" title="live site" /> 
                  </a>
             </footer>
